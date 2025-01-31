b6ee065e489adc0e195a6caa8deed8fd8fc958e8
marina-lucatto <marina.lucatto18@gmail.com>
2025-01-31 12:17:53 -0300

    aula-01

---
+++ javascript/aula-01/READ.md
@@ -0,0 +1,4 @@
+aula introdutória sobre javascript
+
+declaração de variaveis
+Alert, confirm, prompt
---
+++ javascript/aula-01/script.java
@@ -0,0 +1,18 @@
+//console.log("ola mundo");
+
+//window.alert("ola mundo 1");
+
+//alert("ola mundo 2");
+
+//var teste = confirm("olá cliente")
+   //console.log(teste)
+
+//var testePrompt = prompt("digite seu nome")
+//console.log(testePrompt)
+//alert(testePrompt)
+
+//tentativa de soma
+var numero1= Number.parseInt(prompt("digite o primeiro numero"))
+var numero2= Number.parseInt(prompt("digite o segundo numero"))
+console.log(typeof(numero1))
+alert(numero1 + numero2)
